70dfc9603a08cc1cccb94fe0c6c4a177d565975c
shauryamehta96 <58891625+shauryamehta96@users.noreply.github.com>
2019-12-28 22:12:52 +0530

    Updates

---
+++ precache-manifest.81687bde5380d30bf7afd00ba7ef3b62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d85ca15e8bdbd6dd4439246382463a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6d5d7c1554a2655f9bb4",
+    "url": "/monsters-rolodex/static/css/main.0c586c40.chunk.css"
+  },
+  {
+    "revision": "016f4fa70e480c583b00",
+    "url": "/monsters-rolodex/static/js/2.47dc57ea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.47dc57ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d5d7c1554a2655f9bb4",
+    "url": "/monsters-rolodex/static/js/main.c81ba4cd.chunk.js"
+  },
+  {
+    "revision": "7fb4f2586019861475d9",
+    "url": "/monsters-rolodex/static/js/runtime-main.3b8f66f1.js"
+  }
+]);
